bfcd9802bd108ad9a74463801ad042261c377bb5
Anna Tolstova <anna_tolstova@hotmail.com>
2021-08-20 18:27:01 +0300

    start: add basic server

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const PORT = process.env['PORT'] || 5000;
+
+const app = express();
+
+app.get('/', (_, res) => {
+  res.send("Web server works!")
+});
+
+app.listen(PORT, () => {
+  console.log(`Web server started on port ${PORT}`);
+});
