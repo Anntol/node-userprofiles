141e01f093b1d4420380f951646a411f6f445b6e
Anna Tolstova <anna_tolstova@hotmail.com>
2021-08-22 19:42:16 +0300

    feat: allow only images

--- src/middleware/multer.ts
+++ src/middleware/multer.ts
@@ -16,7 +16,21 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  fileFilter: function (_req, file, cb) {
+    const filetypes = /jpeg|jpg|png|gif/;
+  
+    const extname =  filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+  
+    if (mimetype && extname){
+      return cb(null, true);
+    } else {
+      cb(new Error('Provided file should be an image!'));
+    }
+  },
+});
 
 const multerMw = upload.single('photo');
 
